896f14b3b2cea2d49a962ac64d17bf8d1253d7ab
lilawliet <>
2025-06-21 16:26:47 +0800

    feat: image 组件默认不适用 base_path

--- components/Image.tsx
+++ components/Image.tsx
@@ -2,7 +2,7 @@ import NextImage, { ImageProps } from 'next/image'
 
 const basePath = process.env.BASE_PATH
 
-const Image = ({ src, useBasePath = true, ...rest }: ImageProps & { useBasePath?: boolean }) => (
+const Image = ({ src, useBasePath = false, ...rest }: ImageProps & { useBasePath?: boolean }) => (
   <NextImage src={`${useBasePath ? basePath || '' : ''}${src}`} {...rest} />
 )
 
