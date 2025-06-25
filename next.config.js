1472c82a7447c6bcfd87b9d8179a3b5b27c032f9
lilawliet <>
2025-06-25 16:14:27 +0800

    feat: 静态资源 preffix

--- next.config.js
+++ next.config.js
@@ -66,6 +66,7 @@ module.exports = () => {
   return plugins.reduce((acc, next) => next(acc), {
     output,
     basePath,
+    assetPrefix: basePath,
     reactStrictMode: true,
     trailingSlash: false,
     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
