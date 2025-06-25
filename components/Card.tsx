5e5f154fa8ec91f047ab50e67d6a0a05775d7c00
lilawliet <>
2025-06-25 16:24:22 +0800

    feat: test

--- components/Card.tsx
+++ components/Card.tsx
@@ -39,6 +39,7 @@ const CardBody = ({ title, description, imgSrc, href, target }: CardProps) => {
         <Image
           alt={title}
           src={imgSrc}
+          useBasePath={true}
           className="h-32 w-full object-cover object-center transition-transform duration-500 ease-in-out group-hover:scale-110 sm:h-48"
           width={544}
           height={306}
--- scripts/postbuild.mjs
+++ scripts/postbuild.mjs
@@ -1,27 +1,7 @@
-import fs from 'fs'
-import path from 'path'
 import rss from './rss.mjs'
 
 async function postbuild() {
   await rss()
 }
-
-const basePath = process.env.BASE_PATH || ''
-
-function replaceStaticPaths(dir) {
-  fs.readdirSync(dir).forEach((file) => {
-    const fullPath = path.join(dir, file)
-    if (fs.statSync(fullPath).isDirectory()) {
-      replaceStaticPaths(fullPath)
-    } else if (file.endsWith('.mdx')) {
-      let content = fs.readFileSync(fullPath, 'utf-8')
-      // 只替换以 /static/ 开头的资源
-      content = content.replace(/]\(\/static\//g, `](${basePath}/static/`)
-      fs.writeFileSync(fullPath, content)
-    }
-  })
-}
-
-replaceStaticPaths(path.join(process.cwd(), 'data'))
 
 postbuild()
